b8655850ef9287578436092b0fc2a1164fddb40e
Simon D <simondoll.studium@gmail.com>
2021-01-12 21:47:23 +0100

    updates to carla 0.9.9

--- launch_container.sh
+++ launch_container.sh
@@ -1,20 +1,14 @@
-#!/bin/sh
+# this is an easy yet unsecure way to allow the container to access the xserver
+xhost +local:melodic
 
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-touch $XAUTH
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-
-docker run --runtime=nvidia --privileged --rm -it \
-           --volume=$XSOCK:$XSOCK:rw \
-           --volume=$XAUTH:$XAUTH:rw \
-           --volume=$HOME:$HOME \
-           --shm-size=1gb \
-           --env="XAUTHORITY=${XAUTH}" \
-           --env="DISPLAY=${DISPLAY}" \
-           --env=TERM=xterm-256color \
-           --env=QT_X11_NO_MITSHM=1 \
-           --net=host \
-           -u "melodic"  \
-           carla:0.9.8 \
-           bash
+docker run \
+-e SDL_VIDEODRIVER=x11 \
+-e DISPLAY=$DISPLAY \
+--env=TERM=xterm-256color \
+--env=QT_X11_NO_MITSHM=1 \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-p 2000-2002:2000-2002 \
+-it \
+--gpus all \
+--rm \
+ros_carla:0.9.9 bash
